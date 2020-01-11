e0f47509b4ec21c46e282e7e5148b0a6856c66a8
Mikhail Petukhov <petmik@yandex.ru>
2020-01-11 16:08:30 +0300

    Checked authentication with different permissions, created event_api app

--- grustik_client/profiles/views.py
+++ grustik_client/profiles/views.py
@@ -5,10 +5,19 @@ import requests
 
 
 def test_view(request):
-    r = requests.get("http://127.0.0.1:8000/api/profile/")
+    user_token = 'Token ' + request.session.get('token')
+    user_header = {'Authorization': user_token}
+    requestURL = "http://127.0.0.1:8000/api/profile/"
+    user = request.session['user']
 
+    r = requests.get(
+        requestURL,
+        headers=user_header,
+    )
+
+    # print(r.status_code)
     content = r.json()
-    print(len(content))
+    # print(content)
     context = {
         "users_qty": len(content),
         "user_data": content,
@@ -26,14 +35,31 @@ def add_view(request):
     return render(request, 'profiles/add_view.html', context)
 
 def login_view(request):
-    r = requests.post("http://127.0.0.1:8000/api/login/", data={'username': 'test@test.ru', 'password': 'test123'})
+    email = 'test@test.ru'
+    password = 'test123'
+    r = requests.post("http://127.0.0.1:8000/api/login/", data={'username': email, 'password': password})
     status = r.status_code
     if status == 200:
-        message = 'Welcome!'
+        content = r.json()
+        request.session['token'] = content['token']
+        requestURL = "http://127.0.0.1:8000/api/profile/"
+        user_token = 'Token ' + request.session.get('token')
+        user_header = {'Authorization': user_token}
+
+        r = requests.get(
+            requestURL,
+            headers=user_header,
+        )
+        users_list = r.json()
+        user = [item for item in users_list if item['email'] == email][0]
+        # user = next(item for item in users_list if item['email'] == email)
+        # user = next(filter(lambda item: item['email'] == email, users_list))
+        request.session['user'] = user
+        message = 'Welcome, {}!'.format(request.session['user']['name'])
+        # message = 'Welcome!'
     else:
-        message = 'Access denied...'
-    content = r.json()
-    print(content)
+        message = 'Access denied... Please, try again.'
+
     context = {
         'message': message,
         }
